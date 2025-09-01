b693b88d134f8ca4f4d2eb25c8f768cb3d0ba9f1
Everton <evertoncabral.dev1@gmail.com>
2025-08-31 23:47:14 -0300

    feat:Adicionado componentes de consulta

--- src/app/consulta/consulta.ts
+++ src/app/consulta/consulta.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-consulta',
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatIconModule,
+    FormsModule,
+    MatTableModule,
+    MatButtonModule,
+  ],
   templateUrl: './consulta.html',
-  styleUrl: './consulta.scss'
+  styleUrl: './consulta.scss',
 })
-export class Consulta {
-
-}
+export class Consulta {}
