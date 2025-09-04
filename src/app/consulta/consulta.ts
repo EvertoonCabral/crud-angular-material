5a0a5f11c908420b9570972ab33f3058b5e93012
Everton <evertoncabral.dev1@gmail.com>
2025-09-03 23:17:51 -0300

    feat:Finalizado crud de cliente no localstorage

--- src/app/consulta/consulta.ts
+++ src/app/consulta/consulta.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, ɵnormalizeQueryParams } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -9,6 +9,7 @@ import { FormsModule } from '@angular/forms';
 import { MatTableModule } from '@angular/material/table';
 import { ClienteService } from '../service/cliente.service';
 import { Cliente } from '../cadastro/cliente';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-consulta',
@@ -27,11 +28,29 @@ import { Cliente } from '../cadastro/cliente';
 })
 export class Consulta implements OnInit {
   listaClientes: Cliente[] = [];
-  colunasTable: string[] = ['id', 'nome', 'dataNascimento', 'cpf', 'email'];
+  colunasTable: string[] = ['id', 'nome', 'dataNascimento', 'cpf', 'email', 'acoes'];
+  nomeBusca: string = '';
 
-  constructor(private service: ClienteService) {}
+  constructor(private service: ClienteService, private router: Router) {}
 
   ngOnInit() {
+    this.listaClientes = this.service.listarClientes('');
+  }
+
+  pesquisar() {
+    this.listaClientes = this.service.listarClientes(this.nomeBusca);
+  }
+
+  preparaEditar(id: string) {
+    this.router.navigate(['/cadastro'], { queryParams: { id: id } });
+  }
+
+  preparaDeletar(cliente: Cliente) {
+    cliente.deletando = true;
+  }
+
+  deletar(cliente: Cliente) {
+    this.service.deletar(cliente);
     this.listaClientes = this.service.listarClientes('');
   }
 }
--- src/app/service/cliente.service.ts
+++ src/app/service/cliente.service.ts
@@ -9,8 +9,16 @@ export class ClienteService {
 
   constructor() {}
 
-  listarClientes(nome: String): Cliente[] {
-    return this.getStorage();
+  listarClientes(nomeBusca: string): Cliente[] {
+    const clientes = this.getStorage();
+
+    console.log(clientes);
+
+    if (!nomeBusca) {
+      return clientes;
+    } else {
+      return clientes.filter((cliente) => cliente.nome?.indexOf(nomeBusca) !== -1);
+    }
   }
 
   salvar(cliente: Cliente) {
@@ -18,6 +26,14 @@ export class ClienteService {
     storage.push(cliente);
 
     localStorage.setItem(ClienteService.REPO_CLIENTE, JSON.stringify(storage));
+  }
+
+  deletar(cliente: Cliente) {
+    const storage = this.getStorage();
+
+    const novaLista = storage.filter((c) => c.id !== cliente.id);
+
+    localStorage.setItem(ClienteService.REPO_CLIENTE, JSON.stringify(novaLista));
   }
 
   getStorage(): Cliente[] {
@@ -30,5 +46,20 @@ export class ClienteService {
     const clientes: Cliente[] = [];
     localStorage.setItem(ClienteService.REPO_CLIENTE, JSON.stringify(clientes));
     return clientes;
+  }
+
+  buscarClientePorId(id: string): Cliente | undefined {
+    const clientes = this.getStorage();
+    return clientes.find((cliente) => cliente.id === id);
+  }
+
+  atualizar(cliente: Cliente) {
+    const storage = this.getStorage();
+    storage.forEach((c) => {
+      if (c.id === cliente.id) {
+        Object.assign(c, cliente);
+      }
+    });
+    localStorage.setItem(ClienteService.REPO_CLIENTE, JSON.stringify(storage));
   }
 }
